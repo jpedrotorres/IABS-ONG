55c0242ed52186916080332dfceca4074edfadc7
billy <joaodev.pedro@gmail.com>
2025-05-29 20:25:25 -0300

    Alteração no formulário para maior controle dos campos gerados por um formulário django. Além de pequena alteração nos atributos de erro do formulário para mostrar uma mensagem personalizada

--- web_app/contatos/forms.py
+++ web_app/contatos/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
 
 #create forms here
 
 class MembroLoginForms(AuthenticationForm):
-	username= forms.CharField(widget=forms.TextInput(attrs={"class": "input-login", "placeholder": "nome de usuário"}))
-	password= forms.CharField(widget=forms.PasswordInput(attrs={"class": "input-login", "placeholder": "senha"}))
+	error_messages= {
+		"invalid_login": _("usuário ou senha inválidos!"),
+		"inactive": _("essa conta está inativa")
+	}
+
+	username= forms.CharField(
+		label="usuário:",
+		widget=forms.TextInput(attrs={"class": "input-login", "placeholder": "nome de usuário"})
+	)
+	password= forms.CharField(
+		label="senha:",
+		widget=forms.PasswordInput(attrs={"class": "input-login", "placeholder": "senha"})
+	)
