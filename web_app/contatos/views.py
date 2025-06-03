f1fbe2187856ae54dbe3c897fa463fb0f8b3b1dd
João Pedro Torres <158365601+jpedrotorres@users.noreply.github.com>
2025-06-03 17:45:20 -0300

    Correção: Página de reunião estava utilizando o template para a página de mostrar os dados

--- web_app/contatos/views.py
+++ web_app/contatos/views.py
@@ -49,7 +49,13 @@ def parceiro_view(request):
 
 @login_required
 def reuniao_view(request):
-    return render(request, "contatos/reuniao.html")
+    context={
+		"pagina": {
+			"tipo": "reuniao"
+		}
+	}
+
+	return render(request, "contatos/reuniao.html", context)
 
 def logout_confirm_modal_view(request):
 	try:
