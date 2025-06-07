8c9a2121a53dcab8b26a7bae0cbc3934b22ce0d1
Isabelle Pinheiro Lovo <isabelle.pinheirolv@gmail.com>
2025-06-07 00:08:05 -0300

    Final models.py

--- web_app/contatos/models.py
+++ web_app/contatos/models.py
@@ -1,88 +1,118 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import requests
 
 status_membro= [
-	("A", "Ativo"),
-	("I", "Inativo"),
-	("F", "Afastado")
+	('A', 'Ativo'),
+	('I', 'Inativo'),
+	('F', 'Afastado')
 ]
 
 status_parceiro= [
-	("A", "Ativo"),
-	("I", "Inativo")
+	('A', 'Ativo'),
+	('I', 'Inativo')
 ]
 
 status_reuniao= [
-	("R", "Realizada"),
-	("A", "Agendada"),
-	("C", "Cancelada")
+	('R', 'Realizada'),
+	('A', 'Agendada'),
+	('C', 'Cancelada')
 ]
 
 tipo_membro= [
-	("C", "Colaborador")
-	("A", "Administrador")
+	('C', 'Colaborador')
+	('A', 'Administrador')
 ]
 
 tipo_parceiro= [
-    ("PF", "Pessoa Física")
-    ("PJ", "Pessoa Jurídica")
-]
-
-uf_estado = [
-    ("AC", "Acre"),
-    ("AL", "Alagoas"),
-    ("AP", "Amapá"),
-    ("AM", "Amazonas"),
-    ("BA", "Bahia"),
-    ("CE", "Ceará"),
-    ("DF", "Distrito Federal"),
-    ("ES", "Espírito Santo"),
-    ("GO", "Goiás"),
-    ("MA", "Maranhão"),
-    ("MT", "Mato Grosso"),
-    ("MS", "Mato Grosso do Sul"),
-    ("MG", "Minas Gerais"),
-    ("PA", "Pará"),
-    ("PB", "Paraíba"),
-    ("PR", "Paraná"),
-    ("PE", "Pernambuco"),
-    ("PI", "Piauí"),
-    ("RJ", "Rio de Janeiro"),
-    ("RN", "Rio Grande do Norte"),
-    ("RS", "Rio Grande do Sul"),
-    ("RO", "Rondônia"),
-    ("RR", "Roraima"),
-    ("SC", "Santa Catarina"),
-    ("SP", "São Paulo"),
-    ("SE", "Sergipe"),
-    ("TO", "Tocantins")
+    ('PF', 'Pessoa Física')
+    ('PJ', 'Pessoa Jurídica')
 ]
 
 tipo_reuniao= [
-    ("P", "Presencial")
-    ("V", "Virtual")
+    ('P', 'Presencial')
+    ('V', 'Virtual')
+]
+
+uf_estado = [
+    ('AC', 'Acre'),
+    ('AL', 'Alagoas'),
+    ('AP', 'Amapá'),
+    ('AM', 'Amazonas'),
+    ('BA', 'Bahia'),
+    ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'),
+    ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'),
+    ('MA', 'Maranhão'),
+    ('MT', 'Mato Grosso'),
+    ('MS', 'Mato Grosso do Sul'),
+    ('MG', 'Minas Gerais'),
+    ('PA', 'Pará'),
+    ('PB', 'Paraíba'),
+    ('PR', 'Paraná'),
+    ('PE', 'Pernambuco'),
+    ('PI', 'Piauí'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'),
+    ('RS', 'Rio Grande do Sul'),
+    ('RO', 'Rondônia'),
+    ('RR', 'Roraima'),
+    ('SC', 'Santa Catarina'),
+    ('SP', 'São Paulo'),
+    ('SE', 'Sergipe'),
+    ('TO', 'Tocantins')
 ]
 
 class MembroIabs(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     matricula= models.CharField(max_length=15, primary_key=True)
-    tipo= models.CharField(max_length=1, choices=tipo_membro, default="C", blank=True)
+    tipo= models.CharField(max_length=1, choices=tipo_membro, default='C', blank=True)
     nome= models.CharField(max_length=150, blank=True)
     cpf = models.CharField(max_length=11, unique=True, blank=True)
-    status= models.CharField(max_length=1, choices=status_membro, default="A", blank=True)
+    status= models.CharField(max_length=1, choices=status_membro, default='A', blank=True)
     cargo= models.CharField(max_length=70, blank=True, null=True)
     email= models.EmailField(max_length=100, unique=True, blank=True)
-    telefone_celular= models.IntegerField(max_length=11, unique=True, blank=True, null=True)
-    data_nascimento= models.DateField(default=timezone.now, blank=True)
+    telefone_celular= models.CharField(max_length=11, unique=True, blank=True, null=True)
+    data_nascimento= models.DateField(blank=True, null=True)
+
+    def clean(self):
+        if not self.tipo:
+            raise ValidationError({'tipo':'O Membro deve pertencer a um Tipo!'})
+        
+        if self.tipo == 'A' and not self.user:
+            raise ValidationError({'user': 'O Usuário do Membro deve ser informado!'})
+
+        if not self.nome:
+            raise ValidationError({'nome':'O Parceiro deve ter Nome!'})
+
+        if not self.cpf:
+            raise ValidationError({'cpf':'O Parceiro deve ter CPF!'})
+        
+        if not self.status:
+            raise ValidationError({'status':'O Status do Membro deve ser informado!'})
+        
+        if not self.email:
+            raise ValidationError({'email':'O Membro deve ter Email!'})
+        
+        if self.telefone_celular:
+            if not self.telefone_celular.isdigit():
+                raise ValidationError({'telefone_celular':'O Telefone Celular deve conter apenas números!'})
+
+    def save(self, *args, **kwargs):
+        if self.tipo == 'A' and self.user:
+            self.nome = self.user.get_full_name() or self.user.username
+            self.email = self.user.email
+
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
+        if self.user:
+            return self.user.get_full_name() or self.user.username
         return self.nome
-	
-	# verificar: tipo, nome, cpf, status, email
-
-class Usuario(models.Model):
-    nome= models.CharField(max_length=20, primary_key=True)
-    senha= models.CharField(max_length=15, blank=True)
-    membro_adm= models.OneToOneField(MembroIabs, on_delete=models.CASCADE)
 
 class Parceiro(models.Model):
     codigo = models.CharField(max_length=15, primary_key=True)
@@ -91,48 +121,132 @@ class Parceiro(models.Model):
     razao_social= models.CharField(max_length=150, blank=True, null=True)
     cpf= models.CharField(max_length=11, unique=True, blank=True, null=True)
     cnpj= models.CharField(max_length=14, unique=True, blank=True, null=True)
-    status= models.CharField(max_length=1, choices=status_parceiro, default="A", blank=True)
+    status= models.CharField(max_length=1, choices=status_parceiro, default='A', blank=True)
     nome_responsavel= models.CharField(max_length=150, blank=True)
     cargo_responsavel= models.CharField(max_length=70, blank=True, null=True)
     email= models.EmailField(max_length=100, unique=True, blank=True)
-    telefone_fixo= models.IntegerField(max_length=10, unique=True, blank=True, null=True)
+    telefone_fixo= models.CharField(max_length=10, unique=True, blank=True, null=True)
     data_inicio= models.DateField(default=timezone.now, blank=True)
     data_termino= models.DateField(blank=True, null=True)
     segmento= models.CharField(max_length=70, blank=True, null=True)
-    # uf= models.CharField(max_length=2, choices=uf_estado, blank=True)
-    # cep= models.IntegerField(max_length=8, blank=True)
-    # declarar: logradouro
+    uf= models.CharField(max_length=2, choices=uf_estado, blank=True)
+    cep= models.CharField(max_length=8, blank=True)
+    logradouro = models.CharField(max_length=200, blank=True)
+    numero_local= models.CharField(max_length=15, blank=True, null=True)
     website= models.URLField(max_length=200, blank=True, null=True)
     rede_social= models.URLField(max_length=200, blank=True, null=True)
-    # declarar: contrato_parceria= models.FileField(...blank=True, null=True)
+    contrato_parceria= models.FileField(upload_to='parceiro_contratos/', blank=True, null=True)
     observacoes= models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        if self.tipo == "PF":
-            return self.nome
-        elif self.tipo == "PJ":
-            return self.razao_social
+    def clean(self):
+        if not self.tipo:
+            raise ValidationError({'tipo':'O Parceiro deve pertencer a um Tipo!'})
+
+        if self.tipo == 'PF':
+            if not self.nome:
+                raise ValidationError({'nome':'O Parceiro deve ter Nome!'})
+            if not self.cpf:
+                raise ValidationError({'cpf':'O Parceiro deve ter CPF!'})
+            
+        if self.tipo == 'PJ':
+            if not self.razao_social:
+                raise ValidationError({'razao_social':'O Parceiro deve ter Razão Social!'})
+            if not self.cnpj:
+                raise ValidationError({'cnpj':'O Parceiro deve ter CNPJ!'})
+    
+        if self.nome and self.razao_social:
+            raise ValidationError('O Parceiro não pode ter Nome e Razão Social simultaneamente!')
         
-    # verificar: tipo, nome/razao_social, cpf/cnpj, status, email, data_inicio, uf/cep/logradouro
+        if self.cpf and self.cnpj:
+            raise ValidationError('O Parceiro não pode ter CPF e CNPJ simultaneamente!')
+        
+        if not self.status:
+            raise ValidationError({'status':'O Status da parceria deve ser informado!'})
+        
+        if not self.email:
+            raise ValidationError({'email':'O Parceiro deve ter Email!'})
+        
+        if not self.data_inicio:
+            raise ValidationError({'data_inicio':'A Data de Início da parceria deve ser informada!'})
+
+        if self.telefone_fixo:
+            if not self.telefone_fixo.isdigit():
+                raise ValidationError({'telefone_fixo':'O Telefone Fixo deve conter apenas números!'})
+
+        if self.cep:
+            if not self.cep.isdigit():
+                raise ValidationError({'cep':'O CEP deve conter apenas números!'})
+
+            response = requests.get(f'https://viacep.com.br/ws/{self.cep}/json/')
+            data = response.json()
+
+        if 'erro' in data:
+            raise ValidationError({'cep': 'O CEP do Parceiro é inválido!'})
+        
+        self.logradouro = data.get('logradouro', '')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        if self.tipo == 'PF':
+            return self.nome
+        
+        elif self.tipo == 'PJ':
+            return self.razao_social
 
 class Reuniao(models.Model):
     codigo= models.CharField(max_length=15, primary_key=True)
-    status= models.CharField(max_length=1, choices=status_reuniao, default="A", blank=True)
+    status= models.CharField(max_length=1, choices=status_reuniao, default='A', blank=True)
     assunto= models.CharField(max_length=100, blank=True)
     data_hora= models.DateTimeField(default=timezone.now, blank=True)
     tipo= models.CharField(max_length=1, choices=tipo_reuniao, blank=True)
-    # Se tipo == V
     link_conferencia = models.URLField(max_length=200, blank=True, null=True)
-    # Se tipo == P
-    # uf= models.CharField(max_length=2, choices=uf_estado, blank=True, null=True)
-    # cep= models.IntegerField(max_length=8, blank=True, null=True)
-    # declarar: logradouro (blank=True, null=True)
-    
-    # declarar: relatorio= models.FileField(...blank=True, null=True)
+    uf= models.CharField(max_length=2, choices=uf_estado, blank=True, null=True)
+    cep= models.CharField(max_length=8, blank=True, null=True)
+    logradouro = models.CharField(max_length=200, blank=True, null=True)
+    numero_local= models.CharField(max_length=15, blank=True, null=True)
+    relatorio= models.FileField(upload_to='reuniao_relatorios/', blank=True, null=True)
     membros= models.ManyToManyField(MembroIabs)
     parceiros= models.ForeignKey(Parceiro, on_delete=models.CASCADE)
 
+    def clean(self):
+        if not self.status:
+            raise ValidationError({'status':'O Status da Reunião deve ser informado!'})
+
+        if not self.assunto:
+            raise ValidationError({'assunto':'A Reunião deve ter Assunto!'})
+
+        if not self.data_hora:
+            raise ValidationError({'data_hora':'A Reunião deve ter Data e Hora!'})
+
+        if not self.tipo:
+            raise ValidationError({'tipo':'A Reunião deve pertencer a um Tipo!'})
+
+        if self.tipo == 'V':
+            if not self.link_conferencia:
+                raise ValidationError({'link_conferencia':'A Reunião deve ter Link!'})
+
+        if self.tipo == 'P':
+            if not self.uf:
+                raise ValidationError({'uf':'A UF da Reunião deve ser informada!'})
+
+            if self.cep:
+                if not self.cep.isdigit():
+                    raise ValidationError({'cep':'O CEP deve conter apenas números!'})
+
+                response = requests.get(f'https://viacep.com.br/ws/{self.cep}/json/')
+                data = response.json()
+            
+            if 'erro' in data:
+                raise ValidationError({'cep': 'O CEP da Reunião é inválido!'})
+            
+            self.logradouro = data.get('logradouro', '')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.assunto
-
-    # verificar: status, assunto, data_hora, tipo, link_conferencia, uf/cep/logradouro
